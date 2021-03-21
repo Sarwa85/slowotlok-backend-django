4f98996f8339bfacccb8cd8f2f1d519067cf3ca3
Artur Sarwiński <artur.sarwinski@gmail.com>
2021-03-21 21:50:21 +0100

    refaktor nazwy aplikacji polls->api

---
+++ slowotlok/api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Card, Score
+
+admin.site.register(Card)
+admin.site.register(Score)
--- slowotlok/api/migrations/0002_auto_20210321_1043.py
+++ slowotlok/api/migrations/0002_auto_20210321_1043.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polls', '0001_initial'),
+        ('api', '0001_initial'),
     ]
 
     operations = [
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='card',
             name='score',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='polls.score'),
+            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='api.score'),
             preserve_default=False,
         ),
     ]
