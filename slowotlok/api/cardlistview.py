f347cf52b16f81586da5a121c2cf00a371aeecd7
Artur Sarwiński <artur.sarwinski@gmail.com>
2021-03-24 22:38:31 +0100

    Odpytanie o karty z parametrami

--- slowotlok/api/cardlistview.py
+++ slowotlok/api/cardlistview.py
@@ -4,13 +4,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Card, Score
 from .serializers import CardSerializer
-
+import random
 
 class CardListView(APIView):
     http_method_names = ['get', 'post']
 
     def get(self, request: Request):
         c = Card.objects.all()
+
+        if request.query_params.get("order") == "random":
+            c = c.order_by("?")
+
+        c = c.order_by("-score__bad")
+
+        if request.query_params.get("limit"):
+            c = c[:int(request.query_params["limit"])]
+
         return Response(CardSerializer(c, many=True).data, status=status.HTTP_200_OK)
 
     def post(self, request: Request):
